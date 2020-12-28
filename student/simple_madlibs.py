e8e3d1dbf65da9877fb8eff5de67e44e0568d9ad
Soha Jiwani <sohajiwani1@gmail.com>
2020-12-27 21:04:11 -0500

    added in a student template file for the simple_madlibs

---
+++ student/simple_madlibs.py
@@ -0,0 +1,14 @@
+""" 
+In this program, we want it to take in 4 inputs and then print it in the following format
+
+Roses are {adjective}
+{Plural noun} are {color}
+Welcome to {place}
+It's great to have you here!
+
+"""
+
+# TODO: Declare 4 variables where we get the value from the user
+# Here's an example
+
+# TODO: Now print the poem that we haave above, with whatever input we had gotten from the user
